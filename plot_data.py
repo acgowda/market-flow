9d8bfcc22196f2ac394def1136caadaac03d2183
shannan-liu1 <81000747+shannan-liu1@users.noreply.github.com>
2022-06-07 17:07:06 -0700

    Update plot_data.py

--- plot_data.py
+++ plot_data.py
@@ -1,8 +1,11 @@
 import pandas as pd
+import numpy as np
 
-import plotly
+import plotly 
 import plotly.graph_objects as go
 from plotly.subplots import make_subplots
+
+import plotly.express as px
 
 import yfinance as yf
 from ta.trend import MACD
@@ -93,6 +96,50 @@ def plot_ticker(df):
 
     return fig
 
-def plot_returns(returns,predictions):
-        df = pd.dataframe(returns,predictions)
+def plot_returns(returns,predictions,y):
+
+        #datetime stuff
+        dt_all = pd.date_range(start=df.index[0],end=df.index[-1])
+        # retrieve the dates that ARE in the original datset
+        dt_obs = [d.strftime("%Y-%m-%d") for d in pd.to_datetime(df.index)]
+        # define dates with missing values
+        dt_breaks = [d for d in dt_all.strftime("%Y-%m-%d").tolist() if not d in dt_obs]
+
+        # dataframe
+        yhat = predictions
+        df = pd.DataFrame(returns)
+        df['position'] = np.where(yhat == 1,1,-1)
+        df['strategy'] = np.where(y == yhat,1,-1)*df['returns']
+        df['returns_minus_strategy'] = df['returns'] - df['strategy']
+
+        fig = make_subplots(rows=2, cols=1, shared_xaxes=True,
+                        vertical_spacing=0.01, 
+                        row_heights=[0.75,0.25])
         
+        fig.add_trace(go.Scatter(x=df.index, 
+                            y=df['strategy'], 
+                            line=dict(color='green', width=2), 
+                            name='Strategy'),row=1)
+
+        fig.add_trace(go.Scatter(x=df.index, 
+                            y=df['returns'], 
+                            line=dict(color='blue', width=2), 
+                            name='Returns'),row=1)
+
+
+        colors = ['green' if val >= 0 else 'red' for val in df['returns_minus_strategy']]
+        fig.add_trace(go.Bar(x=df.index, 
+                        y=df['returns_minus_strategy'],
+                        marker_color=colors,
+                        name = 'Spread'
+                        ), row=2)
+
+        fig.update_layout(height=500, width=750, 
+                showlegend=False, 
+                xaxis_rangeslider_visible=False,
+                xaxis_rangebreaks=[dict(values=dt_breaks)])
+
+        fig.update_yaxes(title_text="Returns", row=1, col=1)
+        fig.update_yaxes(title_text="Difference", row=3, col=1)
+
+        return fig 
